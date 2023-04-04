3efb03303004cb6dc7d6d11ba7f367eeacca9268
Jay Patel <82361158+jayp-tech@users.noreply.github.com>
2023-04-04 17:01:44 -0400

    CSS change

--- src/components/manager-data-analytics/dashboard-cards-component.jsx
+++ src/components/manager-data-analytics/dashboard-cards-component.jsx
@@ -6,12 +6,11 @@ import './dashboard-cards.css'
 
 export default function DashboardCardsComponent({ payload }) {
     return (
-    
         <div className='wrapper'>
         <div className='dashboard-card-rows'>
             <div className='dashboard-card-columns'>
                 <div className='dashboard-card-cards cards-color-users tooltiptext'>
-                    <div className='icon'>
+                    <div className='icon icon1'>
                         <FontAwesomeIcon icon={faUser} className='users-cards'></FontAwesomeIcon>
                     </div>
                     <div className='Stats'>
@@ -19,14 +18,13 @@ export default function DashboardCardsComponent({ payload }) {
                         <p className='show-data-cards'>{payload.numTotal}</p>
                         <span className="tooltiptext">Total patients registered to the system.</span>
                     </div>
-
                 </div>
             </div>
 
             <div className='dashboard-card-columns'>
                 <div className='dashboard-card-cards cards-color-assessments tooltiptext'>
-                    <div className='icon'>
-                        <FontAwesomeIcon icon={faPersonChalkboard} className='users-cards'></FontAwesomeIcon>
+                    <div className='icon icon2'>
+                     <FontAwesomeIcon icon={faPersonChalkboard} className='users-cards'></FontAwesomeIcon>
                     </div>
                     <div className='Stats'>
                         <h3 style={{ fontSize: "1.7rem" }}>Assessments</h3>
@@ -36,14 +34,13 @@ export default function DashboardCardsComponent({ payload }) {
                 </div>
             </div>
         </div>
-
         <div className='dashboard-card-rows'>
             <div className='dashboard-card-columns'>
                 <div className='dashboard-card-cards cards-color-meeting-counselor tooltiptext'>
-                    <div className='icon'>
-                        <FontAwesomeIcon icon={faUserNurse} className='users-cards'></FontAwesomeIcon>
-                    </div>
-                    <div className='Stats'>
+                  <div className='icon icon3'>
+                     <FontAwesomeIcon icon={faUserNurse} className='users-cards'></FontAwesomeIcon>
+                   </div>
+                   <div className='Stats'>
                         <h3 style={{ fontSize: "1.7rem" }}>Meeting Counselor</h3>
                         <p className='show-data-cards'>{payload.numHasCounselorAppointment}</p>
                         <span className="tooltiptext">Has appointment with counselor</span>
@@ -53,18 +50,17 @@ export default function DashboardCardsComponent({ payload }) {
 
             <div className='dashboard-card-columns'>
                 <div className='dashboard-card-cards cards-color-meeting-doctor tooltiptext'>
-                    <div className='icon' style='color:#d1e9fc;'>
-                        <FontAwesomeIcon icon={faUserDoctor} className='users-cards'></FontAwesomeIcon>
-                    </div>
-                    <div className='Stats'>
-                        <h3 style={{ fontSize: "1.7rem" }}>Meeting Doctor</h3>
-                        <p className='show-data-cards'>{payload.numHasDoctorAppointment}<span>({payload.numInProcessingDoctorAppointment})</span></p>
-                        <span className="tooltiptext">Has appointment with doctor ( Waiting for doctor to process the file )</span>
-                    </div>
+                <div className='icon icon4'> 
+                    <FontAwesomeIcon icon={faUserDoctor} className='users-cards'></FontAwesomeIcon>
+                </div>
+                <div className='Stats'>
+                    <h3 style={{ fontSize: "1.7rem" }}>Meeting Doctor</h3>
+                    <p className='show-data-cards'>{payload.numHasDoctorAppointment}<span>({payload.numInProcessingDoctorAppointment})</span></p>
+                    <span className="tooltiptext"> Has appointment with doctor ( Waiting for doctor to process the file )</span>
+                </div>
                 </div>
             </div>
         </div>
-
         </div>
     )
 }
